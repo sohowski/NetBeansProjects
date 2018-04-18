7bb71f262336a7b58336f6d47af2e624b83c296b
RF <RF@192.168.1.100>
2018-04-18 02:15:19 +0200

    First version of Random WFRP 1st ed. characteristics.

---
+++ LosowanieWFRP/src/losowaniewfrp/LosowanieWFRP.java
@@ -0,0 +1,39 @@
+
+package losowaniewfrp;
+
+import java.util.Random;
+
+/**
+ *
+ * @author RF
+ */
+public class LosowanieWFRP {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        int suma = 0;
+        int wynik = 0;
+        Random rzut = new Random();
+        System.out.println(" WW  US  I  Zr  CP  Int Op SW  Ogd");
+        for (int i = 0; i < 9; i++) {
+            
+            wynik=((rzut.nextInt(10)+1))+(rzut.nextInt(10)+1);
+            suma+=wynik;
+            System.out.print(" " + wynik + " ");
+            
+        }
+        
+        System.out.println(" suma: " + suma);
+        
+        int wynikk3 = 0;
+        for (int i = 0; i < 3; i++) {
+            wynikk3 = (rzut.nextInt(3)+1);
+            System.out.print(" k3: " + wynikk3);
+        }
+        
+    }
+    
+}
